fa3fd88f828ad2b0fe1f7a3a9eb45cfd9983fd7f
Nguyen <58890310+nguyenj-c@users.noreply.github.com>
2021-12-20 17:18:33 +0100

    PHPUnit avec les test du winner

--- project/MorpionTest.php
+++ project/MorpionTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-/*
+require __DIR__ . '/../vendor/autoload.php';
 use PHPUnit\Framework\TestCase;
 require 'Morpion.php';
 
@@ -17,16 +17,160 @@ final class MorpionTest extends TestCase
         ];
         if(gettype($board) == 'string'){
             $board = $morpion->convertString($board);
-        } else if()
-        assertEquals($board,$board2);
+        } 
+        $this->assertEquals($board,$board2);
     }
-}
-$test = new MorpionTest();
-$test->testMorpionType();
-*/
-require 'Morpion.php';
-final class MorpionTest{
-    function test(){
+
+    public function line(){
+        return [
+            ['O', 'O', 'O'],
+            ['X', 'X', 'O'],
+            ['O', 'X', 'X'],
+        ];
+    }
+
+    public function winningDiagonals(){
+        return [
+            'diag2' =>[
+                [
+                    ['X', 'X', 'O'],
+                    ['X', 'O', 'O'],
+                    ['O', 'O', 'X'],
+                ],
+                'O',
+            ],
+            'strDiag1' =>[
+                 "XXOXOOOOX",
+                 'O',
+            ],
+ 
+            'diag1' =>[
+                [
+                    ['X', 'O', 'O'],
+                    ['O', 'X', 'O'],
+                    ['O', 'O', 'X'],
+                ],
+                'X',
+            ]
+        ];
+    }
+    public function winningLine(){
+        return [
+            'line' =>[
+                [
+                    ['O', 'O', 'O'],
+                    ['X', 'X', 'O'],
+                    ['O', 'X', 'X'],
+                ],
+                'O'
+            ],            
+            'line2' =>[
+                [
+                    ['X', 'X', 'O'],
+                    ['O', 'O', 'O'],
+                    ['O', 'X', 'X'],
+                ],
+                'O'
+            ],
+            'line3' =>[
+                [
+                    ['X', 'X', 'O'],
+                    ['O', 'X', 'X'],
+                    ['O', 'O', 'O'],
+                ],
+                'O'
+            ]
+        ];
+    }
+    public function winningColumn(){
+        return [
+            'column' =>[
+                [
+                    ['O', 'X', 'O'],
+                    ['O', 'X', 'X'],
+                    ['O', 'O', 'X'],
+                ],
+                'O',
+            ],
+            'column2' =>[
+                [
+                    ['X', 'O', 'X'],
+                    ['X', 'O', 'O'],
+                    ['O', 'O', 'X'],
+                ],
+                'O',    
+            ],
+            'column3' =>[
+                [
+                    ['O', 'X', 'O'],
+                    ['X', 'X', 'O'],
+                    ['X', 'O', 'O'],
+                ],
+                'O',   
+            ]
+        ];
+    }
+    public function winningTie(){
+        return [
+            'tie1' =>[
+                [
+                    ['O', 'X', 'O'],
+                    ['X', 'X', 'O'],
+                    ['O', 'O', 'X'],
+                ],
+                'tie',
+            ],
+            'tie2' =>[
+                [
+                    ['X', 'X', 'O'],
+                    ['O', 'O', 'X'],
+                    ['X', 'O', 'X'],
+                ],
+                'tie',    
+            ],
+            'tie3' =>[
+                [
+                    ['O', 'X', 'O'],
+                    ['O', 'X', 'X'],
+                    ['X', 'O', 'O'],
+                ],
+                'tie',   
+            ]
+        ];
+    }
+    /**
+     * @dataProvider winningDiagonals
+     */
+    public function testWinningDiagonals($grid, $winner)
+    {
+        $morpion = new Morpion();
+        $this->assertEquals($morpion->andTheWinnerIs($grid), $winner);
+    }
+    /**
+     * @dataProvider winningLine
+     */
+    public function testWinningLine($grid, $winner)
+    {
+        $morpion = new Morpion();
+        $this->assertEquals($morpion->andTheWinnerIs($grid), $winner);
+    }
+    /**
+     * @dataProvider winningColumn
+     */
+    public function testWinningColumn($grid, $winner)
+    {
+        $morpion = new Morpion();
+        $this->assertEquals($morpion->andTheWinnerIs($grid), $winner);
+    }
+    /**
+     * @dataProvider winningTie
+     */
+    public function testWinningTie($grid, $winner)
+    {
+        $morpion = new Morpion();
+        $this->assertEquals($morpion->andTheWinnerIs($grid), $winner);
+    }
+    /*function test(){
         $morpion = new Morpion();
         $diag1 = [
             ['X', 'O', 'O'],
@@ -40,12 +184,12 @@ final class MorpionTest{
             ['O', 'O', 'X'],
         ];
         $column = "XOXXOOXOO";
-        $morpion->andTheWinnerIs($diag1);
-        $morpion->andTheWinnerIs($diag2);
-        $morpion->andTheWinnerIs($line);
-        $morpion->andTheWinnerIs($column);
-
-    }
+        echo($morpion->andTheWinnerIs($diag1));
+        echo($morpion->andTheWinnerIs($diag2));
+        echo($morpion->andTheWinnerIs($line));
+        echo($morpion->andTheWinnerIs($column));
+    }*/
 }
+
 
 ?>
