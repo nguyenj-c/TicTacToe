6e16a93234bf92f805f10a49f09f85becd7c2c92
Nguyen <58890310+nguyenj-c@users.noreply.github.com>
2021-12-21 15:36:58 +0100

    Ajout test 19/20

--- project/Morpion.php
+++ project/Morpion.php
@@ -22,50 +22,49 @@ final class Morpion
             return "";
         }
     }
-
-    function affichage($board)
-    {
-        if ($this->verificarBoard($board) == true) {
-            echo '<table>';
-            $boucle = 0;
-            foreach ($board as $value) {
-                echo '<tr>';
-                foreach ($value as $player) {
-                    $boucle += 1;
-                    if ($boucle != 3) {
-                        $boucle = 0;
-                        echo '<td>';
-                        print_r($player);
-                        echo '</td>';
-                    }
+    function verifDiagonale($board){
+        $nbligne = count($board);
+        $ligne = $nbligne-1;
+        for($i = 0; $i < $ligne ; --$i){
+            $k = $i;
+            $j = $ligne;
+            while($k != 1){
+                if (($board[$k][$k] == $board[$j][$j]) && $k-1 == 0) {
+                    $str = $board[$k][$k];
+                    return $str;
                 }
-                echo '</tr>';
-            }
-            echo '</table>';
-            echo"<br>";
-        } else if ($this->verificarBoard($board) == false) {
-            $tableau = $this->convertString($board);
-            echo '<table>';
-            $boucle = 0;
-            foreach ($tableau as $value) {
-                echo '<tr>';
-                foreach ($value as $player) {
-                    $boucle += 1;
-                    if ($boucle != 3) {
-                        $boucle = 0;
-                        echo '<td>';
-                        print_r($player);
-                        echo '</td>';
-                    }
-
+                if (($board[$k-$k][$k] == $board[$k+1-$k][$k-$k+1]) && $k-$k == 0) {
+                    $str = $board[$k+1-$k][$k+1-$k];
+                    return $str;
                 }
-                echo '</tr>';
+                ++$k;
+                --$j;
             }
-            echo '</table>';
-            echo"<br>";
-        } else {
-            echo "Ce n'est ni une chaîne de caractères ni un tableau";
         }
+    } 
+    function verifLine_Column($board){
+        $nbligne = count($board);
+        $ligne = $nbligne-1;
+        for($i = 0; $i < $ligne; ++$i){
+            $k = $ligne;
+            $j = $i;
+            while($k == $ligne){
+                    if (($board[$k][$i] == $board[$j][$i])) {
+                        if($j == $k){
+                            $str = $board[$k][$i];
+                            return $str;
+                        }
+                    } 
+                    if(($board[$i][$k] == $board[$i][$j])) {
+                        if($j == $k){
+                            $str = $board[$i][$k];
+                            return $str;
+                        }
+                    }
+                    ++$j;
+                    --$k;
+            }
+        } 
     }
 
     function andTheWinnerIs($board): string
@@ -73,67 +72,71 @@ final class Morpion
         if ($this->verificarBoard($board) == false) {
             $board2 = $this->convertString($board);
             $nbligne = count($board2);
-            foreach ($board2 as $column) {     
-                if ($column[0] == $column[1] && $column[1] == $column[2]) {
-                    $str = $board[0];
-                    return $str;
+            if($nbligne = 3){
+                foreach ($board2 as $column) {     
+                    if ($column[0] == $column[1] && $column[1] == $column[2]) {
+                        $str = $column[0];
+                        return $str;
+                    }
                 }
-            }
-            $j = 0;
-            for($i = 0; $i < 3; ++$i){
-                $k = $j;
-                if ($board2[$k][$i] == $board2[$k+1][$i] && $board2[$k+1][$i] == $board2[$k+2][$i]) {
-                    $str = $board[$k][$i];
-                    return $str;
+                $j = 0;
+                for($i = 0; $i < 3; ++$i){
+                    $k = $j;
+                    if ($board2[$k][$i] == $board2[$k+1][$i] && $board2[$k+1][$i] == $board2[$k+2][$i]) {
+                        $str = $board2[$k][$i];
+                        return $str;
+                    } 
+                    ++$k;
                 } 
-                ++$k;
-            } 
-            if ($board2[0][0] == $board2[1][1] && $board2[1][1] == $board2[2][2]) {
-                $str = $board[1][1];
-                return $str;
-            } else if ($board2[2][0] == $board2[1][1] && $board2[1][1] == $board2[0][2]) {
-                $str = $board2[1][1];
-                return $str;
-            } else {
-                return " tie";
+                if ($board2[0][0] == $board2[1][1] && $board2[1][1] == $board2[2][2]) {
+                    $str = $board2[1][1];
+                    return $str;
+                } else if ($board2[2][0] == $board2[1][1] && $board2[1][1] == $board2[0][2]) {
+                    $str = $board2[1][1];
+                    return $str;
+                } else {
+                    return "tie";
+                }
+            }         
+            else if($nbligne > 3){
+                $this->verifLine_Column($board);
+                $this->verifDiagonale($board);
             }
         }
         else if ($this->verificarBoard($board) == true) {
-            $nbligne = count($board);
-            foreach ($board as $column) {
-
-                if ($column[0] == $column[1] && $column[1] == $column[2]) {
-                    $str = $column[0];
-                    return $str;
+            $nbligne2 = count($board);
+            if($nbligne2 = 3){
+                foreach ($board as $column) {     
+                    if ($column[0] == $column[1] && $column[1] == $column[2]) {
+                        $str = $column[0];
+                        return $str;
+                    }
                 }
-            }
-            $j = 0;
-            for($i = 0; $i < $nbligne-1; ++$i){
-                $k = $j;
-                while($k < $nbligne && nbligne == 0){
+                $j = 0;
+                for($i = 0; $i < 3; ++$i){
+                    $k = $j;
                     if ($board[$k][$i] == $board[$k+1][$i] && $board[$k+1][$i] == $board[$k+2][$i]) {
                         $str = $board[$k][$i];
                         return $str;
                     } 
                     ++$k;
-                }
-            } 
-            $i = $nbligne-1;
-            while($i == 0){
-                if ($board[$i][$i] == $board[$i-1][$i-1] && $board[$i-1][$i-1] == $board[$i-2][$i-2] && $i-2 == 0) {
-                    $str = $board[$i][$i];
+                } 
+                if ($board[0][0] == $board[1][1] && $board[1][1] == $board[2][2]) {
+                    $str = $board[1][1];
                     return $str;
-                } else if ($board[$i-$i][$i] == $board[$i+1-$i][$i-$i+1] && $board[$i+1-$i ][$i-$i+1] == $board[$i][$i-$i] && $i-$i+1 == 1) {
-                    $str = $board[$i+1-$i][$i+1-$i];
+                } else if ($board[2][0] == $board[1][1] && $board[1][1] == $board[0][2]) {
+                    $str = $board[1][1];
                     return $str;
-                } else{
-                    --$i;
+                } else {
+                    return "tie";
                 }
             }
-        }else {
-            return "tie";
+            else if($nbligne2 > 3){
+                $this->verifLine_Column($board);
+                $this->verifDiagonale($board);
+            }
         }
         return "tie";
     }
-}
+}    
 ?>
