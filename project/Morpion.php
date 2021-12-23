28ed51a7d4c14b5fa99c8ae97a0720127f169cd1
Nguyen <58890310+nguyenj-c@users.noreply.github.com>
2021-12-23 10:47:22 +0100

    refactor: Remove some if conditions

--- project/Morpion.php
+++ project/Morpion.php
@@ -4,24 +4,11 @@ final class Morpion
 {
     function convertString($board)
     {
-        if(strlen($board) == 9){
-            $arr = str_split($board);
-            $tableau = array_chunk($arr, 3, false);
-            return $tableau;
-        } else{
-            return 'Format invalide';
-        }
+        $arr = str_split($board);
+        $tableau = array_chunk($arr, 3, false);
+        return $tableau;
     }
-    function verificarBoard($board)
-    {
-        if (is_array($board)) {
-            return true;
-        } else if(gettype($board) == 'string')  {
-            return false;
-        }else{
-            return "";
-        }
-    }
+
     function verifDiagonale($board){
         $nbligne = count($board);
         $ligne = $nbligne-1;
@@ -36,56 +23,60 @@ final class Morpion
                 break;
             }   
         } 
+        return 'tie';
     }
-
 
     function verifLine_Column($board){
         $nbligne = count($board);
         $ligne = $nbligne-1;
-        foreach($board as $value){
-            for($i = 0; $i < $nbligne; ++$i){
-                if (($value[$i] == $value[$ligne])){
-                    $str = $value[$i];
-                    return $value[$i];
+        for($i = 0; $i < $nbligne; ++$i){
+            for($j = $ligne; $j+1 < $nbligne; ++$j){
+                if (($board[$i][$j] != $board[$i][$j+1])){
+                    break;
                 }
-                else if(($board[$i][$value] == $board[$i][$ligne])){
-                        $str = $board[$i][$value];
-                        return $board[$i][$value];
-                } 
-                ++$j;
+                if($i == $ligne){
+                    $str = $board[$i][$j];
+                }
             }
-        }     
+        } 
+        for($i = 0; $i < $nbligne; ++$i){
+            for($j = $ligne; $j+1 < $nbligne; ++$j){
+                if (($board[$j][$i] != $board[$j+1][$i])){
+                    break;
+                }
+                if($i  == $ligne){
+                    $str = $board[$j][$i];
+                    return $str;
+                }
+            }
+        }
     } 
-        
+    function winner($board){
+        $this->verifDiagonale($board);
+        $this->verifLine_Column($board);
+    }
 
     function andTheWinnerIs($board): string
     {
-        if (!$this->verificarBoard($board)) {
+        if (!is_array($board)) {
             $board2 = $this->convertString($board);
             $nbligne = count($board2);
-            if($nbligne >= 3){
-                $str = $this->verifDiagonale($board2);
-                return $str;
-            }
+            $str = $this->verifDiagonale($board2);
+            return $str;
         }
-        else if ($this->verificarBoard($board)) {
-            $nbligne2 = count($board);
-            if($nbligne2 >= 3){
-                $str1 = $this->verifDiagonale($board);
-                $str = $this->verifDiagonale($board);
-                if(isset($str1)){
-                    $result = $str1;
-                }else if(isset($str)){
-                    $result = $str;
-                }
-                else
-                {
-                    $result = "tie";
-                }
-                return $result; 
-            }
-        }    
-        return "tie";
+        $nbligne2 = count($board);
+        $str1 = $this->verifLine_Column($board);
+        $str = $this->verifDiagonale($board);
+        if(isset($str)){
+            $result = $str;
+        }else if(isset($str1)){
+            $result = $str1;
+        }
+        else
+        {
+            $result = "tie";
+        }
+        return $result;     
     }
 }    
 ?>
